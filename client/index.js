4ec4ff49a8088195f65d91ee5c8babc35bfd1469
Kyle Anson <kyle.anson7@gmail.com>
2017-04-21 18:18:24 -0400

    Init dump

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import app from './app.jsx';
+
+ReactDOM.render(
+  <app />, document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    publicPath: '/client/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/,
+        query: { presets: ['es2015', 'react'] }
+      },
+      {
+        test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/,
+        query: { presets: ['es2015', 'react'] }
+      }
+    ]
+  }
+};
